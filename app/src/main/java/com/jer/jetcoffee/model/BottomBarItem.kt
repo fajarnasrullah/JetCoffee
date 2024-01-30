6596163dd2906168d2cd736772178d44256350b2
fajar <nashrulljr01@gmail.com>
2024-01-30 11:23:23 +0700

    commit for jetCoffee mobile app, using jetpack compose

---
+++ app/src/main/java/com/jer/jetcoffee/model/BottomBarItem.kt
@@ -0,0 +1,9 @@
+package com.jer.jetcoffee.model
+
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomBarItem (
+    val title: String,
+    val icon: ImageVector
+)
---
+++ app/src/main/java/com/jer/jetcoffee/ui/components/HomeSection.kt
@@ -0,0 +1,19 @@
+package com.jer.jetcoffee.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+
+@Composable
+fun HomeSection(
+    title: String,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column (modifier) {
+        SectionText(title, modifier)
+        content()
+    }
+
+}
